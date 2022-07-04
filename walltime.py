0b5123b84b2965f9c93d06d1943769e5b54491ea
levi makwei <levi.d.makwei@vanderbilt.edu>
2022-07-04 00:25:07 +0000

    parsing json

--- walltime.py
+++ walltime.py
@@ -1,8 +1,13 @@
 import requests
+import pprint
 
 # Test API by sending a Get requests
 url = 'https://s3.amazonaws.com/data-production-walltime-info/production/dynamic/walltime-info.json?now=1528962473468.679.0000000000873'
 response = requests.get(url)
+r = response.json()
 
 #printing JSON to confirm success
-print(response.json)
+# print(r)
+
+#
+pprint.pprint(r, width=40)
